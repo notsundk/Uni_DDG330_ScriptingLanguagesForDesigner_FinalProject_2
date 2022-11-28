9a003fb77dc1e45635d803e3031a5b472861b579
Peerawich Sinamornwech <sunpeerawich@gmail.com>
2022-11-29 04:46:06 +0700

    ButtonJuice

--- Assets/Scripts/MainMenuButton.cs
+++ Assets/Scripts/MainMenuButton.cs
@@ -1,20 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuButton : MonoBehaviour
 {
+    public Image StartButtonImage;
     public Sprite ButtonDefault;
     public Sprite ButtonPressed;
 
     void Start()
     {
-        
+        StartButtonImage.sprite = ButtonDefault;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ButtonPress()
     {
-        
+        StartButtonImage.sprite = ButtonPressed;
+        Invoke("ButtonUnPress", 0.125f);
+        Invoke("LoadScene", 0.35f);
+    }
+
+    private void ButtonUnPress()
+    {
+        StartButtonImage.sprite = ButtonDefault;
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
