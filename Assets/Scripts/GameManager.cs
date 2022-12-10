53548e07d98f15f0bf5a125bc033c6db3cf3cb5f
Peerawich Sinamornwech <sunpeerawich@gmail.com>
2022-12-10 12:24:04 +0700

    Update (Interval for spawn)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEditor.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,10 +14,14 @@ public class GameManager : MonoBehaviour
     public int Score_Best;
 
     [Header("UI ELEMENTS")]
+    // Gameplay
     public TextMeshProUGUI Gameplay_ScoreUI;
     public Slider Gameplay_Time;
+
+    // Endscreen
     public TextMeshProUGUI Endscreen_ScoreUI;
     public TextMeshProUGUI Endscreen_BestScoreUI;
+    public GameObject Endscreen_NewBestScoreText;
 
     [Header("UI")]
     public GameObject Canvas_Gameplay;
@@ -24,12 +29,30 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        // CHECK SYS
         Debug.Log("Game is being played on " + SystemInfo.deviceType);
+
+        // SET TIME
         Time_Cur = Time_Max;
+        
+        // SET RECORED BEST SCORE
+        Score_Best = PlayerPrefs.GetInt("BestScore");
+        Debug.Log("Best Score: " + Score_Best);
+
+        // TURN OFF FAN FARE
+        Endscreen_NewBestScoreText.SetActive(false);
     }
 
     private void Update()
     {
+
+        // RESET BEST SCORE TO 0
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PlayerPrefs.SetInt("BestScore", Score_Cur);
+            Debug.Log("RESET HIGH SCORE");
+        }
+
         // LIVE SCORE
         Gameplay_ScoreUI.text = Score_Cur.ToString();
 
@@ -40,8 +63,24 @@ public class GameManager : MonoBehaviour
         // GAME OVER STATE
         if (Time_Cur <= 0)
         {
+            // ENDSCREEN OVERLAY + PAUSE
+            Time.timeScale = 0f;
             Canvas_Gameplay.SetActive(false);
             Canvas_Endscreen.SetActive(true);
+
+            // CHECK FOR NEW BEST SCORE
+            if (Score_Cur > Score_Best)
+            {
+                PlayerPrefs.SetInt("BestScore", Score_Cur);
+                Score_Best = PlayerPrefs.GetInt("BestScore");
+                Endscreen_NewBestScoreText.SetActive(true);
+            }
+
+            // DISPLAY CUR SCORE
+            Endscreen_ScoreUI.text = Score_Cur.ToString();
+
+            // DISPLAY BEST SCORE
+            Endscreen_BestScoreUI.text = Score_Best.ToString();
         }
     }
 
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+
+// FOR ENEMY
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject _slimeEnemy;
-    [SerializeField] private float _spawnInterval;
+
+    [Header("Spawn Setting")]
+    [SerializeField] private float _initialSpawnInterval;
+    [SerializeField] private float _minSpawnInterval;
+    [SerializeField] private float _maxSpawnInterval;
+    [SerializeField] private float _curSpawnInterval;
 
     private void Start()
     {
-        StartCoroutine(SpawnEnemy(_spawnInterval, _slimeEnemy));
+        // INITIAL COROUTINE START
+        StartCoroutine(SpawnEnemy(_initialSpawnInterval, _slimeEnemy));
     }
 
     private IEnumerator SpawnEnemy(float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
         GameObject newEnemy = Instantiate(enemy, transform.position, Quaternion.identity);
+
+        // START NEXT COROUTINE
+        _curSpawnInterval = Random.Range(_minSpawnInterval, _maxSpawnInterval);
+        interval = _curSpawnInterval;
         StartCoroutine(SpawnEnemy(interval, enemy));
     }
 }
